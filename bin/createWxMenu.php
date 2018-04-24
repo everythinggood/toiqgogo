c3426a16d3b70a47236a7399c19633f9746bb170
ycy <helloyi@xiyanghui.com>
2018-04-24 18:40:25 +0800

    fix WxScanPush

---
+++ bin/createWxMenu.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ycy
+ * Date: 4/24/18
+ * Time: 6:02 PM
+ */
+
+require __DIR__.'/../vendor/autoload.php';
+
+
+if(!file_exists(__DIR__.'/../cache/access_token')){
+    throw new Exception('access_token is not found!');
+}
+
+$access_token = file_get_contents(__DIR__.'/../cache/access_token');
+
+
+
+$client = new \GuzzleHttp\Client();
+
+$response = $client->request('POST','https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$access_token,[
+    'body'=>json_encode([
+        "button"=>[
+            [
+                "type"=>"scancode_push",
+                "name"=>"扫码领取纸巾",
+                "key"=>"scan_get_paper"
+            ]
+        ]
+    ],JSON_UNESCAPED_UNICODE)
+]);
+
+var_export($response->getBody()->getContents());
