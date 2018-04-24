9c9c29b674b6cd1f34f650a998d8ddd8a2a30cba
ycy <helloyi@xiyanghui.com>
2018-04-24 17:47:34 +0800

    fix WxScanPush

--- src/Action/WxScanPushAction.php
+++ src/Action/WxScanPushAction.php
@@ -10,6 +10,7 @@ namespace Action;
 
 
 use Contract\Container;
+use GuzzleHttp\Client;
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -23,10 +24,15 @@ class WxScanPushAction implements ActionInterface
      * @var Logger
      */
     private $logger;
+    /**
+     * @var Client
+     */
+    private $client;
 
     public function __construct(ContainerInterface $container)
     {
         $this->logger = $container[Container::NAME_LOGGER];
+        $this->client = new Client();
     }
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
@@ -36,7 +42,43 @@ class WxScanPushAction implements ActionInterface
 
         $this->logger->addInfo('WxScanPush---XML:  '.$request->getBody()->getContents());
 
+        $content = $request->getBody()->getContents();
+
+        $arr = $this->convertXMLToArr($content);
+
+        if($toUser = $arr['FromUserName']){
+            $this->sendCustomMessage($toUser);
+        }
+
+
         return $response;
+    }
+
+    protected function convertXMLToArr($content){
+
+        libxml_disable_entity_loader(true);
+        $arr = json_decode(json_encode(simplexml_load_string($content, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+
+        return $arr;
+    }
+
+    protected function sendCustomMessage(String $toUser){
+        $fileName = __DIR__.'/../../cache/access_token';
+        if(file_exists(__DIR__.'/../../cache/access_token')){
+            $access_token = file_get_contents($fileName);
+
+            $response = $this->client->request('POST', 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' . $access_token ,[
+                'json' => [
+                    'touser' => $toUser,
+                    'msgtype' => 'text',
+                    'text' => [
+                        'content' => '请点击【<a href="http://m.zhiwei99.com/addon/YiKaTong/GuanzhuGzh/up?state=412 ">免费领取纸巾</a >】'
+                    ]
+                ]
+            ]);
+
+            $this->logger->addInfo('sendCustomMessage',json_decode($response->getBody()->getContents(),true));
+        }
     }
 
 }
