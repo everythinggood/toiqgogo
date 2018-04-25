1829229c8af0e2469801eadde1fcb6ff2b6142dd
ycy <helloyi@xiyanghui.com>
2018-04-25 11:30:39 +0800

    fix WxScanPush a标签 can not set

--- src/Action/WxScanPushAction.php
+++ src/Action/WxScanPushAction.php
@@ -69,14 +69,17 @@ class WxScanPushAction implements ActionInterface
         if(file_exists(__DIR__.'/../../cache/access_token')){
             $access_token = file_get_contents($fileName);
 
+            /**
+             * 神经病 腾讯 a标签href要带双引号 ，并且a标签后面不能带空格
+             */
             $response = $this->client->request('POST', 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' . $access_token ,[
-                'json' => [
+                'body' => json_encode([
                     'touser' => $toUser,
                     'msgtype' => 'text',
                     'text' => [
-                        'content' => '请点击【<a href="http://m.zhiwei99.com/addon/YiKaTong/GuanzhuGzh/up?state=412 ">免费领取纸巾</a >】'
+                        'content' => "请点击【<a href=\"http://m.zhiwei99.com/addon/YiKaTong/GuanzhuGzh/up?state=412\">免费领取纸巾</a>]"
                     ]
-                ]
+                ],JSON_UNESCAPED_UNICODE)
             ]);
 
             $this->logger->addInfo('sendCustomMessage success',json_decode($response->getBody()->getContents(),true));
