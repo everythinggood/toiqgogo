11a5523430238f5e4bbccf58d79eafc0a8d77c98
ycy <helloyi@xiyanghui.com>
2018-04-27 19:16:59 +0800

    test

--- bin/test.php
+++ bin/test.php
@@ -8,15 +8,15 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$content = <<<XML
-<xml><URL><![CDATA[http://toiqgogo.com/wxScanPush]]></URL><ToUserName><![CDATA[gh_89a2cde84fbe]]></ToUserName><FromUserName><![CDATA[oX8Ie1c4jNeN1qfXvQ9cat4Gmk8U]]></FromUserName><CreateTime>1524557199</CreateTime><MsgType><![CDATA[event]]></MsgType><Event><![CDATA[SCAN]]></Event><Latitude></Latitude><Longitude></Longitude><Precision></Precision><MsgId>22222</MsgId></xml>
-XML;
+//$content = <<<XML
+//<xml><URL><![CDATA[http://toiqgogo.com/wxScanPush]]></URL><ToUserName><![CDATA[gh_89a2cde84fbe]]></ToUserName><FromUserName><![CDATA[oX8Ie1c4jNeN1qfXvQ9cat4Gmk8U]]></FromUserName><CreateTime>1524557199</CreateTime><MsgType><![CDATA[event]]></MsgType><Event><![CDATA[SCAN]]></Event><Latitude></Latitude><Longitude></Longitude><Precision></Precision><MsgId>22222</MsgId></xml>
+//XML;
+//
+//
+//libxml_disable_entity_loader(true);
+//$json = json_decode(json_encode(simplexml_load_string($content, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
 
-
-libxml_disable_entity_loader(true);
-$json = json_decode(json_encode(simplexml_load_string($content, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
-
-$FromUserName = $json['FromUserName'];
+$FromUserName = 'o2QbF1ehRjXUteuLmmef1X2kXap0';
 
 $client = new \GuzzleHttp\Client();
 
